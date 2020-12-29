d3e6836039fa4a8a40a545b5f6135806f1ecc506
inu255 <artmvhdv@mail.ru>
2020-12-29 19:12:24 +0700

    Всё кроме карты

---
+++ js/works.js
@@ -0,0 +1,6 @@
+new ScrollBooster({
+  viewport: document.querySelector('.works__slider'),
+  content: document.querySelector('.works__slider'),
+  scrollMode: 'native',
+  direction: 'horizontal'
+});
